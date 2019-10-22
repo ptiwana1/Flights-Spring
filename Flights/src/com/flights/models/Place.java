0f6be8781b197b17927a447088954c2a9283d2ab
Parneet Kaur <parneet21001145@gmail.com>
2019-10-21 22:49:40 -0700

    added models

---
+++ Flights/src/com/flights/models/Place.java
@@ -0,0 +1,36 @@
+package com.flights.models;
+
+public class Place {
+	private String name;
+	private String country;
+	private String currency;
+	private String locale;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public String getLocale() {
+		return locale;
+	}
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+	
+	
+
+}
